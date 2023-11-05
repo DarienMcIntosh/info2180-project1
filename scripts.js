5b0299365ba6365172ffb832bda6e5e4baa3d5e1
Darien McIntosh <darienmcintosh14@gmail.com>
2023-11-05 13:28:00 -0500

    Added updated project files

--- scripts.js
+++ scripts.js
@@ -1,1 +1,23 @@
 /* Add your JavaScript to this file */
+document.addEventListener("DOMContentLoaded", function() {
+    const form = document.querySelector("form");
+    const emailInput = document.getElementById("email");
+    const messageDiv = document.querySelector(".message");
+
+    form.addEventListener("submit", function(event) {
+        event.preventDefault();
+
+        const email = emailInput.value.trim();
+
+        if (email === "") {
+            // If no email address is entered
+            messageDiv.textContent = "Please enter a valid email address.";
+        } else {
+            // If a valid email address is entered
+            messageDiv.textContent = `Thank you! Your email address ${email} has been added to our mailing list!`;
+            // Clear the input field after submission
+            emailInput.value = "";
+        }
+    });
+});
+
